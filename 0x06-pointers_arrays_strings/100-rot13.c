1fcb05fa76822dfe5cdffb4bf40866b9a369b119
Adesodun-Gbenga <adesodungbengajo@gmail.com>
2022-10-26 17:26:32 -0700

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,20 @@
 
 char *rot13(char *s)
 {
-	int i = 0, j = 0;
-	char alp[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	while (s[i])
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; alp[j] && alp[j] != s[i]; j++);
-		if (alp[j])
-			s[i] = rot[j];
-		i++;
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
 	}
 	return (s);
 }
